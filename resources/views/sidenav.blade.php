ec65c35d00f04bf32811a36c5e5bd65ea6f75ab1
HooiErn <d200217c.sc.edu.my>
2022-10-18 12:34:13 +0800

    viewBlacklist complete

--- resources/views/sidenav.blade.php
+++ resources/views/sidenav.blade.php
@@ -1,6 +1,5 @@
-
-  <!--SideNav-->       
-  @if(auth()->user()->isAdmin())
+<!--SideNav-->       
+@if(auth()->user()->isAdmin())
   <div id="mySideNav" class="sidenav">
             <ul style="padding-left:10px;">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>    
@@ -8,6 +7,9 @@
             <li><a href="{{ route('user.register') }}">Register Member</a></li> 
             <li><a href="{{ route('agent.show') }}">Agents Information</a></li> 
             <li><a href="{{ route('member.show') }}">Members Information</a></li>
+            {{-- <li><a href="{{ route('agent.view') }}">Agents List</a></li> --}}
+            {{-- <li><a href="{{ route('member.view') }}">Members List</a></li> --}}
+            <li><a href="{{ route('blacklist.view') }}">BlackList</a></li>
             </ul>
             </div>
             @elseif(auth()->user()->isAgent())
@@ -16,12 +18,18 @@
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>    
             <li><a href="{{ route('user.register') }}">Register Member</a></li> 
             <li><a href="{{ route('member.show') }}">Members Information</a></li>
+            {{-- <li><a href="{{ route('agent.view') }}">Agents List</a></li> --}}
+            {{-- <li><a href="{{ route('member.view') }}">Members List</a></li> --}}
+            <li><a href="{{ route('blacklist.view') }}">BlackList</a></li>
             </ul>
             </div>
             @elseif(auth()->user()->isMember())
             <div id="mySideNav" class="sidenav">
             <ul style="padding-left:10px;">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>     
+            {{-- <li><a href="{{ route('agent.view') }}">Agents List</a></li> --}}
+            {{-- <li><a href="{{ route('member.view') }}">Members List</a></li> --}}
+            <li><a href="{{ route('blacklist.view') }}">BlackList</a></li>
             </ul>
             </div>
             @endif
