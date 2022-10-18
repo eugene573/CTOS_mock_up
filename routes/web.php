9a52e9fb87ac84d058d58eceeb12f26e3164d9ea
HooiErn <d200217c.sc.edu.my>
2022-10-18 11:38:44 +0800

    blacklist error update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Pages\BlacklistController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 
 /*
@@ -22,13 +22,13 @@ Route::get('/', function () {
 });
 
 Route::get('/', [HomeController::class, 'index']);
+
 //Route for authenticating users' login and registration
 Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
-Route::get('registration', [AuthController::class, 'registration'])->name('register');
-Route::get('userRegistration', [AuthController::class, 'userRegistration'])->name('user.register');
 Route::get('agent-registration', [AuthController::class, 'agentRegistration'])->name('agent.register');
-Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('user-registration', [AuthController::class, 'userRegistration'])->name('user.register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
 
 //Route for editing and updating users' information
 Route::get('agent-edit/{id}', [AuthController::class, 'editAgent'])->name('agent.edit');
@@ -37,19 +37,17 @@ Route::post('update' ,[AuthController::class,'update'])->name('user.update');
 Route::get('show-agent',[AuthController::class, 'showAgent'])->name('agent.show');
 Route::get('show-member',[AuthController::class, 'showMember'])->name('member.show');
 
-
 //Route for handling matter of blacklisting
-Route::get('add-to-blacklist',[BlacklistController::class, 'addToBlacklist'])->name('add.to.blacklist');
+Route::get('add-to-blacklist/{id}',[BlacklistController::class, 'addToBlacklist'])->name('add.to.blacklist');
 Route::post('post-blacklist',[BlacklistController::class, 'add'])->name('blacklist.post');
+Route::get('view-blacklist',[BlacklistController::class, 'viewBlacklist'])->name('blacklist.view');
 
 Route::get('profile', [AuthController::class, 'profile'])->name('profile.view'); 
 Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard'); 
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-
 
 //Route for providing support if user forget password
 Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
 Route::post('forget-password', [ForgotPasswordController::class, 'sendForgetPasswordForm'])->name('forget.password.post'); 
 Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
 Route::post('reset-password', [ForgotPasswordController::class, 'sendResetPasswordForm'])->name('reset.password.post');
-
