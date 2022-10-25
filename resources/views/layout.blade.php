c9bce8696267e56eb5978efff5683ad858b8b04e
HooiErn <d200217c.sc.edu.my>
2022-10-25 12:50:04 +0800

    update navbartop layout and link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -149,7 +149,7 @@
             </div>
             @endif
         <!--TopNav-->
-        <a class="navbar-brand" href="{{ route('dashboard') }}" style="color:white;"> CTOS Mock-Up</a>
+        <a class="navbar-brand" href="#" style="color:white;"> CTOS Mock-Up</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
