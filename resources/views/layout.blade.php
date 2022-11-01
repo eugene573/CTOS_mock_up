d2525edef678fa33439dfa5193c4c47dc523008f
HooiErn <d200217c.sc.edu.my>
2022-11-01 10:35:29 +0800

    navbar update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -151,7 +151,7 @@
             </div>
             @endif
         <!--TopNav-->
-        <a class="navbar-brand" href="#" style="color:white;"> CTOS Mock-Up</a>
+        <a class="navbar-brand" href="#" style="color:white;"> CTOS Website</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
